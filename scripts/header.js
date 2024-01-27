f8d7091d0a71928b5a94844e204472040f43926d
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-01-27 08:50:45 -0500

    Add reflection questions, update header so it's clear what page you're at, make the links in the Research page not just go to the page, but the highlighted text on the page

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,16 @@
-document.head.innerHTML = document.head.innerHTML + `<link rel="stylesheet" type="text/css" href="/styles/header.css" />`
+document.head.innerHTML = document.head.innerHTML + `<link rel="stylesheet" type="text/css" href="/styles/header.css" />`;
 
 document.body.innerHTML =
 `<div id="header">
 <a href="/">
     <h3 id="logo">FINANCE <br /> FINDER</h3>
 </a>
-<a href="/" class="header-link">Home</a>
-<a href="/about" class="header-link">About</a>
-<a href="/research" class="header-link">Research</a>
+<a href="/" class="header-link" id="home-link">Home</a>
+<a href="/about" class="header-link" id="about-link">About</a>
+<a href="/research" class="header-link" id="research-link">Research</a>
+<a href="/reflection-questions" class="header-link" id="reflection-questions-link">Reflection Questions</a>
 <a href="/Finance Finder Presentation.pdf" class="header-link" target="_blank">Presentation</a>
 </div>`
 + document.body.innerHTML;
+
+document.getElementById(document.currentScript.getAttribute("link")).style.borderBottom = "4px solid rgb(93, 0, 255)";
