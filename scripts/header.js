59529328c97597cde03ca5c859f8b369dc94ba8e
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-02-15 18:34:46 -0500

    Split Research page into Problem and Solution, use slightly better practices

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,6 @@
 document.head.innerHTML = document.head.innerHTML + `<link rel="stylesheet" type="text/css" href="/styles/header.css" />
-<link rel="icon" type="image/x-icon" href="/favicon.png" />`;
+<link rel="icon" type="image/x-icon" href="/favicon.png" /> <meta name="viewport" content="width=device-width,initial-scale=1" />
+<meta name="description" content="Manage your finances and budget in a smarter way, with Finance Finder." />`;
 
 document.body.innerHTML =
 `<div id="header">
@@ -7,9 +8,10 @@ document.body.innerHTML =
     <h3 id="logo">FINANCE<br>FINDER</h3>
 </a>
 <a href="/" class="header-link" id="home-link">Home</a>
+<a href="/problem" class="header-link" id="problem-link">Problem</a>
+<a href="/solutions" class="header-link" id="solutions-link">Solutions</a>
 <a href="/desktop" class="header-link" id="desktop-link">Desktop</a>
 <a href="/mobile" class="header-link" id="mobile-link">Mobile</a>
-<a href="/research" class="header-link" id="research-link">Research</a>
 <a href="/process" class="header-link" id="process-link">Process</a>
 <a href="/reflection-questions" class="header-link" id="reflection-questions-link">Reflection Questions</a>
 <a href="/about" class="header-link" id="about-link">About</a>
