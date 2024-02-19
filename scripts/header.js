9455d96a24c5971195e19154cc9b9d37afe722a8
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-02-19 09:55:14 -0500

    Tolerate more devices
    Hamburger menu, hero image breaks to next line

--- scripts/header.js
+++ scripts/header.js
@@ -7,14 +7,17 @@ document.body.innerHTML =
 <a href="/">
     <h3 id="logo">FINANCE<br>FINDER</h3>
 </a>
-<a href="/" class="header-link" id="home-link">Home</a>
-<a href="/problem" class="header-link" id="problem-link">Problem</a>
-<a href="/solutions" class="header-link" id="solutions-link">Solutions</a>
-<a href="/desktop" class="header-link" id="desktop-link">Desktop</a>
-<a href="/mobile" class="header-link" id="mobile-link">Mobile</a>
-<a href="/process" class="header-link" id="process-link">Process</a>
-<a href="/reflection-questions" class="header-link" id="reflection-questions-link">Reflection Questions</a>
-<a href="/about" class="header-link" id="about-link">About</a>
+<div id="header-link-div">
+    <a href="/" class="header-link" id="home-link">Home</a>
+    <a href="/problem" class="header-link" id="problem-link">Problem</a>
+    <a href="/solutions" class="header-link" id="solutions-link">Solutions</a>
+    <a href="/desktop" class="header-link" id="desktop-link">Desktop</a>
+    <a href="/mobile" class="header-link" id="mobile-link">Mobile</a>
+    <a href="/process" class="header-link" id="process-link">Process</a>
+    <a href="/reflection-questions" class="header-link" id="reflection-questions-link">Reflection Questions</a>
+    <a href="/about" class="header-link" id="about-link">About</a>
+</div>
+<button id="hamburger-button">&#9776;</button>
 </div>` + document.body.innerHTML;
 
 document.getElementById(document.currentScript.getAttribute("link")).style.color = "rgb(204, 0, 163)";
@@ -23,4 +26,17 @@ document.getElementById("header").style.transitionDuration = "0s"
 document.getElementById("header").addEventListener("mouseleave", function() {
     document.getElementById("header").style.transitionDuration = ".4s";
     setTimeout(document.getElementById("header").style.transitionDuration = "0", 400);
+});
+
+let blockOrNone;
+document.getElementById("hamburger-button").addEventListener("click", function() {
+    if (document.querySelector(".header-link").style.display == "block") {
+        blockOrNone = "none";
+    } else {
+        blockOrNone = "block";
+    }
+
+    for (let i = 0; i < document.getElementsByClassName("header-link").length; i++) {
+        document.getElementsByClassName("header-link")[i].style.display = blockOrNone;
+    }
 });
